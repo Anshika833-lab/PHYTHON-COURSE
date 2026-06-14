1cc0bcbdf6c160665d98902476be134ae92f64f2
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-06-14 11:45:47 +0100

    Variables

---
+++ programming/variable.py
@@ -0,0 +1,13 @@
+#creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#user input
+name = input("enter your name: ")
+
+print("\nhello", name, "\nwelcome to codingal")
