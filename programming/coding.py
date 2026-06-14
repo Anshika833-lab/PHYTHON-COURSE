f9c45f630d0d23beddded36d84b4a82bc1ef3162
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-06-14 11:30:59 +0100

    coding

---
+++ programming/coding.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("welcome to the world of programming ")
+
+#print a number
+print(4)
+
+#next line after print
+print("hello python \nwelcome")
+
+#pass multiple arguments for printing
+print("hello" ,5)
+
+#end arguement of print statement
+print("welocome to" , end="@")
