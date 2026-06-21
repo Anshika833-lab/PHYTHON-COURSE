360528a71ead2fd24da7e3fa486ba52802b574d7
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-06-21 11:44:51 +0100

    Type casting

---
+++ data types/Typecasting.py
@@ -0,0 +1,20 @@
+name = "penguin" 
+age = 15
+is_student = True
+weight = 38.5
+
+print("data type of name is" , type(name))
+print("Data type of age is", type(age))
+print("data type od is_student is", type(is_student))
+print("data type of weight is", type(weight))
+
+print("/n after type casting....")
+age=str(age)
+print(age)
+print("data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is", type(weight))
+
+
+      
