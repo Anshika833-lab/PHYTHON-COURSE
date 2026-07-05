df2baaf95734a42275d3d25f02107287727c69f6
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-07-05 11:43:25 +0100

    odd and even

---
+++ conditional_statement/odd__even.py
@@ -0,0 +1,7 @@
+number = int(input("enter number to check:8"))
+print("Number to be checked:" ,number )
+
+if number%2!=0:
+    print("this is an odd number")
+else:
+    print("this is an even number")
