4f96963213490464411707a238a00b7e41a42ea7
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-07-22 11:54:34 +0100

    Operator precedence

---
+++ Phython challenges/operator_precedence.py
@@ -0,0 +1,16 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) *x/y;
+print("Value of (v+w)*x/y", z)
+
+
+name = "Alex"
+age = 0
+
+if name == "Alex" or name == "john" and age >= 2:
+    print("hello! welcome.")
+else :
+    print("good bye!!")
