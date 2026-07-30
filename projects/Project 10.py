3ccf4c1b6f41dc31b8dc68b1318beec45bfa75b3
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-07-30 11:44:25 +0100

    Power calculator

---
+++ projects/Project 10.py
@@ -0,0 +1,10 @@
+print("***************************")
+print("WELCOME TO POWER CALCULATOR")
+print("***************************")
+
+base_num = int(input("enter a number:"))
+power_num = int(input("Enter a power:"))
+
+print(base_num, "raised to the power of", power_num, "is", base_num ** power_num)
+
+
