e695e6c160d3a546e9f0f4af11bcdfd3744f0a4a
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-08-10 13:07:17 +0100

    Star Pyramid

---
+++ projects/project 13.py
@@ -0,0 +1,12 @@
+print("====STAR PYRAMID====")
+n = int(input("enter the amonut of rows:"))
+
+number = 1
+for row in range (n):
+    for j in range (1, row+1):
+        print("*", end=" ")
+        number =+1
+    print()
+
+    
+
