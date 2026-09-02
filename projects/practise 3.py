2f6393c35d879c23f0cab3600dc967f14ffb9cc9
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-09-02 12:06:21 +0100

    Function calculator

--- projects/practise 3.py
+++ projects/practise 3.py
@@ -1,27 +1,46 @@
-print("FUNCTION CALCULATOR")
+print("Welcome to Function calculator!")
+def add(a,b):
+    return a+b
+def subtract(a, b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a, b):
+    return a/b
 
-while True:
-    print("\n1. Add 5")
-    print("2. Subtract 5")
-    print("3. Exit")
+try:
+    num1 = float(input("Enter the first number:"))
+    num2 = float(input("Enter the second number:"))
+except ValueError:
+    print("Enter a valid number!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
 
-    choice = int(input("Choose an operation: "))
+print("1. Addition")
+print("2. Substraction")
+print("3. multiplication")
+print("4. Division")
 
-    if choice == 3:
-        print("Thank you!")
-        break
+choice = input("Enter your choice:(1, 2, 3 or 4)")
 
-    if choice != 1 and choice != 2:
-        print("Invalid choice!")
-        continue
+if choice == "1":
+    answer = add(num1, num2)
+    print("The answer is:", answer)
 
-    x = int(input("Enter the value of x: "))
+elif choice == "2":
+    answer = subtract(num1, num2)
+    print("The answer is:", answer)
 
-    if choice == 1:
-        answer = x + 5
-    elif choice == 2:
-        answer = x - 5
+elif choice == "3":
+    answer = multiply(num1, num2)
+    print("The answer is:", answer)
 
-    print("the answer is", answer)
+elif choice == "4":
+    answer = divide(num1, num2)
+    print("The answer is:", answer)
+    
 
-    print("=====================")
+
+else:
+    print("Invalid input!")
+    
