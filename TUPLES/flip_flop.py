2f95c8125f046078717ddc68e5c96994f543d74c
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-09-13 11:31:38 +0100

    Flip Flop

---
+++ TUPLES/flip_flop.py
@@ -0,0 +1,17 @@
+def pallind(r):
+    e = len(r) -1
+    s = 0
+    while(s<e):
+      if(r[s]!=r[e]):
+          return False
+      s+=1
+      e-=1
+    return True
+
+
+r = (1,2,3,3,2,1)
+if(pallind(r)):
+   print("the tuple is Flip-flop")
+else:
+   print("The tuple is not Flip-flop")
+   
