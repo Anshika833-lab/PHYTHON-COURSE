56395f74dd6782b33aa75994b45cf8125f1d56d9
Anshika833-lab <deoprakashtiwari833@gmail.com>
2026-09-20 09:31:27 +0100

    getride duplicate

---
+++ All_about_dictionary/getride_duplicate.py
@@ -0,0 +1,18 @@
+student_data = {
+    "id1": {"name": "Alice", "class": "V", "subject_integration": ["english", "Math", "Science"]},
+    "id2": {"name": "Bob", "class": "V", "subject_integration": ["english", "Math", "Science"]},
+    "id3": {"name": "Charlie", "class": "V", "subject_integration": ["english", "Math", "Science"]},
+    "id4": {"name": "Bob", "class": "V", "subject_integration": ["english", "Math", "Science"]}
+}
+
+result = {}
+seen = set()
+
+for student_id, details in student_data.items():
+    unique_key = (details["name"], details["class"], tuple(details["subject_integration"]))
+    if unique_key not in seen:
+        seen.add(unique_key)
+        result[student_id] = details
+
+for k , v in result.items():
+    print (k , ":" , v)
